ad11b1505da6e7ccbf221c50c78faaa6b8b60575
lukasz-gradowski <58110065+lukasz-gradowski@users.noreply.github.com>
2019-11-23 15:32:20 +0100

    0.01
    0.01

---
+++ 4/app/src/main/java/com/example/a4/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.a4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button zaloguj;
+    EditText login, haslo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = (EditText) findViewById(R.id.editText8);
+        haslo = (EditText) findViewById(R.id.editText7);
+        zaloguj = (Button) findViewById(R.id.button2);
+
+        /*zaloguj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (login.getText().toString().isEmpty() || haslo.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Enter the Data", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login - " + login.getText().toString() + " \n" + "Hasło - " + haslo.getText().toString()
+                           , Toast.LENGTH_SHORT).show();
+                }
+            }
+        });*/
+
+        zaloguj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                String pierwsza = login.getText().toString();
+                String druga = haslo.getText().toString();
+                if(pierwsza.equals("lukasz")&&druga.equals("pluto12")){
+
+                    intent = new Intent(MainActivity.this, WypActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+    /*public void click(View view) {
+        Intent intent;
+        if(view.getId()==R.id.button2){
+            intent=new Intent(MainActivity.this, wyp.class);
+            startActivity(intent);
+
+
+        }
+    }*/
+}
