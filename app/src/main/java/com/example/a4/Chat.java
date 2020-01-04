95149592ba792adfc546ef977cd54379bbc2bc33
iEmix <emdom4@gmail.com>
2020-01-04 12:29:51 +0100

    dziala

--- app/src/main/java/com/example/a4/Chat.java
+++ app/src/main/java/com/example/a4/Chat.java
@@ -2,7 +2,10 @@ package com.example.a4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Chat extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class Chat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        SharedPreferences sharedPref = getSharedPreferences("dane", Context.MODE_PRIVATE);
+        String txt = sharedPref.getString("login", "");
+        TextView nick = findViewById(R.id.nick);
+        nick.setText("Twój nick to: "+txt);
     }
 }
