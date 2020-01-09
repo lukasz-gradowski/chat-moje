b85a8a6dad3345fcc46f68f61191b2fe29cef041
iEmix <emdom4@gmail.com>
2020-01-09 20:11:21 +0100

    poprawki

--- app/src/main/java/com/example/a4/ScrollingActivity.java
+++ app/src/main/java/com/example/a4/ScrollingActivity.java
@@ -13,7 +13,6 @@ public class ScrollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
