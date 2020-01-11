e608894bd606ed3657a69929b3ca6bde69a91a2c
iEmix <emdom4@gmail.com>
2020-01-11 08:28:03 +0100

    poprawki

--- app/src/main/java/com/example/a4/MainActivity.java
+++ app/src/main/java/com/example/a4/MainActivity.java
@@ -11,14 +11,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import org.mindrot.jbcrypt.BCrypt;
-
 import java.util.HashMap;
 import java.util.Map;
 
