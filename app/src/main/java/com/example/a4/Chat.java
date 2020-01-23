3a0a8d28da9a98c3dfdc17cb768843ec5b93846f
iEmix <emdom4@gmail.com>
2020-01-23 20:32:53 +0100

    Revert "poprawki"
    This reverts commit 3b93bc4dbfa6263298fa727f6c10be24ae9182a6.

--- app/src/main/java/com/example/a4/Chat.java
+++ app/src/main/java/com/example/a4/Chat.java
@@ -107,9 +107,7 @@ public class Chat extends AppCompatActivity {
         LinearLayout content = findViewById(R.id.content);
         TextView messages = new TextView(this);
         messages.setBackgroundColor(getRandomColor());
-        ViewGroup.LayoutParams params;
-        params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        messages.setLayoutParams(params);
+        messages.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         chatItems.add(messages);
         messages.setText(chatItems.toString());
         messages.setText(Html.fromHtml(msg));
